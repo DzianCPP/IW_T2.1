cc8c77e32943d5ad5544cdab88b01dfdd28e9c97
DzianCPP <ocherkiprozaika@gmail.com>
2022-11-02 16:45:18 +0300

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <title>AddUserApp</title>
+</head>
+<body>
+<form>
+    <label for="email">E-mail</label>
+    <input type="text" name="email" class="emailInput" id="mainPageEmailInput" placeholder="Enter your email here"><br>
+</form>
+</body>
+
+</html>
