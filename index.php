d24623fcda0525d7ad90914e415bebee18dfdb1f
DzianCPP <ocherkiprozaika@gmail.com>
2022-11-02 21:30:44 +0300

    Updated style of the form

--- index.php
+++ index.php
@@ -2,34 +2,13 @@
 <html lang="en-US">
 
 <head>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="style.css">
     <title>AddUserApp</title>
 </head>
 
 <body>
-    <form method="POST" action="">
-        <label for="email">E-mail</label>
-        <input type="text" class="InputText" name="email" placeholder="Enter your email"><br>
-
-        <label for="name">Your first and last name</label>
-        <input type="text" class="InputText" name="name" placeholder="Enter your first and last name"><br>
-
-        <label for="gender">Gender</label>
-        <select name="gender" class="InputSelect" id="inputSelectGender">
-            <option>Male</option>
-            <option>Female</option>
-            <option>Transgender</option>
-            <option>Non-binary</option>
-            <option>Other</option>
-        </select><br>
-
-        <label for="status">Status</label>
-        <select name="status" class="InputSelect" id="inputSelectStatus">
-            <option>Active</option>
-            <option>Inactive</option>
-        </select>
-    </form>
-
+    <h1 class="MainPageHeader" id="mainPageHeader">AddUserApp</h1>
+    <?php include_once "new.html"; ?>
 </body>
 
 </html>
