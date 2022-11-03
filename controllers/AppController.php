fac3fab8c6eb78c44a6a0c8f0bb167b672254036
DzianCPP <ocherkiprozaika@gmail.com>
2022-11-03 14:04:12 +0300

    Restructured the app

--- controllers/AppController.php
+++ controllers/AppController.php
@@ -1,7 +1,10 @@
 <?php
 
 namespace Controller;
+
 include_once "UserController.php";
+include_once "UserAdder.php";
+
 use UserController\UserController as UC;
 
 class AppController {
--- index.php
+++ index.php
@@ -1,18 +1,3 @@
-<!DOCTYPE html>
-<html lang="en-US" dir="ltr">
+<?php
 
-<head>
-    <link rel="icon" type="image/x-icon" href="favicon.ico">
-    <meta charset="utf-8">
-    <link rel="sript" type="script" href="script.js">
-    <link rel="stylesheet" href="style.css">
-    <title>AddUserApp</title>
-</head>
-
-<body>
-
-    <script> location.href="new.html"; </script>
-
-</body>
-
-</html>
+$app = new App();
