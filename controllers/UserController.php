5d94b6a1ce0fa13b20eb964e6d661a1a58a82507
DzianCPP <ocherkiprozaika@gmail.com>
2022-11-04 17:07:22 +0300

    Changes in the logic of the app

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -1,26 +1,19 @@
 <?php
-
+require_once "../system/base-paths.php";
 include AUTOLOAD_PATH;
 
 class UserController
 {
     public function create()
     {
-        $newUserData = array(
-            $_POST['email'],
-            $_POST['name'],
-            $_POST['gender'],
-            $_POST['status']
-        );
-
-        foreach ($newUserData as $newUserDataField) {
-            echo "<script> console.log($newUserDataField); </script>";
-        }
-
         $this->new();
     }
 
     public function new()
     {
+        require_once APP_PATH;
     }
 }
+
+$userController = new UserController();
+$userController->create();
