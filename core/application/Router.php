b4c2d428f2cd92573f4d67ef6185ec1ef72dfff9
DzianCPP <ocherkiprozaika@gmail.com>
2022-11-08 10:36:50 +0300

    AppCOntroller instead of main

--- core/application/Router.php
+++ core/application/Router.php
@@ -12,21 +12,16 @@ class Router
     {
         $routes = require_once "../bootstrap/routes.php";
 
-        if (array_key_exists('REQUEST_URI', $_SERVER)) {
-            $request_uri = $_SERVER['REQUEST_URI'];
-
-            if ($request_uri === '' || $request_uri === '/') {
-                $this->track = new Track('main', 'index', '');
-            } else {
-                $request_uri = trim($request_uri);
-                $slashPosition = strpos($request_uri, '/', 0);
-                $controller = substr($request_uri, 0, $slashPosition);
-                $action = substr($request_uri, $slashPosition, strlen($request_uri));
-
-                $this->track = new Track($controller, $action, '');
-            }
+        if (!array_key_exists("REQUEST_URI", $_SERVER) || $_SERVER['REQUEST_URI'] === '' || $_SERVER['REQUEST_URI'] === '/') {
+                $this->track = new Track('AppController', 'index', '');
         } else {
-            $this->track = new Track('main', 'index', '');
+            $request_uri = $_SERVER['REQUEST_URI'];
+            $request_uri = trim($request_uri);
+            $slashPosition = strpos($request_uri, '/', 0);
+            $controller = substr($request_uri, 0, $slashPosition);
+            $action = substr($request_uri, $slashPosition, strlen($request_uri));
+
+            $this->track = new Track($controller, $action, '');
         }
     }
 
