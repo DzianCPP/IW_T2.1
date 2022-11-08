caaf60163776a9f3dd353bd1e261426c07b18c20
DzianCPP <ocherkiprozaika@gmail.com>
2022-11-08 17:39:45 +0300

    Removed redunant functionality from Track class

--- core/application/Track.php
+++ core/application/Track.php
@@ -4,34 +4,15 @@ namespace core\application;
 
 class Track
 {
-    private string $controller = "";
-    private string $action = "";
-    private $method = '';
-    private string $controllerActionPath;
+    private string $controller = '';
+    private string $action = '';
+    private string $method = '';
 
     public function __construct(array $route)
     {
         $this->controller = $route['controller'];
         $this->action = $route['action'];
         $this->method = $route['method'];
-        $this->setControllerActionPath();
-    }
-
-    private function setControllerActionPath(): void
-    {
-        $this->controllerActionPath = CONTROLLERS_PATH . $this->controller;
-        $this->controllerActionPath .= ucfirst($this->action);
-        $this->controllerActionPath .= ".php";
-    }
-
-    public function getControllerActionPath(): string
-    {
-        return $this->controllerActionPath;
-    }
-
-    public function getControllerPath(): string
-    {
-        return CONTROLLERS_PATH . $this->controller . ".php";
     }
 
     public function getActionName(): string
