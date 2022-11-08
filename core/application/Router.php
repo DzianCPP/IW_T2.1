d44af653b09a49520740d0bca27b23f8160544e9
DzianCPP <ocherkiprozaika@gmail.com>
2022-11-08 14:18:52 +0300

    Router checks if the request method is valid

--- core/application/Router.php
+++ core/application/Router.php
@@ -14,7 +14,9 @@ class Router
 
         if (!array_key_exists("REQUEST_URI", $_SERVER) || $_SERVER['REQUEST_URI'] === '' || $_SERVER['REQUEST_URI'] === '/') {
             $route = '';
-            $this->track = new Track($routes[$route]);
+            if ($this->methodValid($route)) {
+                $this->track = new Track($routes[$route]);
+            }
         } else {
             $query_string = $_SERVER['QUERY_STRING'];
             $request_route = ltrim($_SERVER['REQUEST_URI'], '/');
@@ -23,12 +25,22 @@ class Router
                 $request_route = substr($request_route, 0, $questionMarkPosition);
             }
             $request_route = rtrim($request_route, '/');
-            $this->track = new Track($routes[$request_route]);
+            if ($this->methodValid($routes[$request_route])) {
+                $this->track = new Track($routes[$request_route]);
+            }
         }
     }
 
     public function getTrack(): Track
     {
         return $this->track;
+    }
+
+    private function methodValid($route): bool
+    {
+        if ($_SERVER['REQUEST_METHOD'] === strtoupper($route['method'])) {
+            return true;
+        }
+        return false;
     }
 }
