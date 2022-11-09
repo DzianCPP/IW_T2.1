5b007f38bb0ecc8ac54a912e4a99eb36c412eed7
DzianCPP <ocherkiprozaika@gmail.com>
2022-11-09 17:04:14 +0300

    Modified DBConnection class

--- core/application/DBConnection.php
+++ core/application/DBConnection.php
@@ -1,8 +1,30 @@
 <?php
 
 namespace core\application;
+use PDO;
 
 class DBConnection
 {
+    private string $userName = 'root';
+    private string $hostName = 'localhost';
+    private string $password = '';
+    private string $databaseName = 'UsersDatabase'; //todo mode all the credentials to a protected env file
+    private PDO $connection;
 
+    public function __construct()
+    {
+        $dsn = "mysql:host=$this->hostName;dbname=$this->databaseName";
+        $this->connection = new PDO(
+            $dsn,
+            $this->userName,
+            $this->password);
+    }
+
+    public function getConnection(): PDO
+    {
+        return $this->connection;
+    }
 }
+
+
+
