15de1a8b9ce39b7a79f1fc3a7fd6cd10b3725407
DzianCPP <ocherkiprozaika@gmail.com>
2022-11-10 09:53:12 +0300

    Application class now has a private instance of Database class

--- core/application/Application.php
+++ core/application/Application.php
@@ -4,10 +4,13 @@ namespace core\application;
 
 class Application
 {
+    private Router $router;
+    private Database $database;
+
     public function run(): void
     {
-        $router = new Router();
-        $track = $router->getTrack();
+        $this->router = new Router();
+        $track = $this->router->getTrack();
         $controllerName = $track->getControllerName();
         $controllerObject = new $controllerName;
         $actionName = $track->getActionName();
