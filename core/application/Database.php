3c6e232c078dc1f1cb2ead418355d45ed25f7830
DzianCPP <ocherkiprozaika@gmail.com>
2022-11-14 12:21:42 +0300

    Changed credential usage in the Database class

--- core/application/Database.php
+++ core/application/Database.php
@@ -2,27 +2,35 @@
 
 namespace core\application;
 use PDO;
+use Dotenv\Dotenv;
 
 class Database
 {
-    private string $userName = 'root';
-    private string $hostName = 'localhost';
-    private string $password = 'pass';
-    private string $databaseName = 'UsersDatabase'; //todo move all the credentials to a protected .env file
     private PDO $connection;
+    private array $env = [];
 
     public function __construct()
     {
-        $dsn = "mysql:host=$this->hostName;dbname=$this->databaseName";
-        $this->connection = new PDO(
-            $dsn,
-            $this->userName,
-            $this->password);
+        $this->env = $this->getDotEnv();
+        $dbHostName = $this->env['DB_HOST_NAME'];
+        $dbPassword = $this->env['DB_PASSWORD'];
+        $dbUserName = $this->env['DB_USER_NAME'];
+        $dbName = $this->env['DB_NAME'];
+        $dsn = "mysql:host=".$dbHostName.";dbname=".$dbName;
+
+        $this->connection = new PDO($dsn, $dbUserName, $dbPassword);
     }
 
     public function getConnection(): PDO
     {
         return $this->connection;
+    }
+
+    private function getDotEnv(): array
+    {
+        $dotenv = Dotenv::createImmutable(BASE_PATH);
+        $dotenv->safeLoad();
+        return $_ENV;
     }
 }
 
