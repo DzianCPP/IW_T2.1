e2be419ade0c89891ec1d489b5815ac49a53c3bc
DzianCPP <ocherkiprozaika@gmail.com>
2022-11-23 17:06:06 +0300

    Added a base abstract class for View -> UsersView

--- core/controllers/BaseController.php
+++ core/controllers/BaseController.php
@@ -1,18 +1,18 @@
 <?php
 
 namespace core\controllers;
-use core\view\View;
+use core\view\UsersView;
 use core\models\Model;
 use core\models\Users;
 
 class BaseController
 {
     protected Users $users;
-    protected View $view;
+    protected UsersView $view;
 
     protected function setView(string $templatePath): void
     {
-        $this->view = new View($templatePath);
+        $this->view = new UsersView($templatePath);
     }
 
     protected function setModel(): void
--- core/view/View.php
+++ core/view/View.php
@@ -2,20 +2,14 @@
 
 namespace core\view;
 
-class View
+abstract class View
 {
-    private string $templatePath;
+    protected string $templatePath;
 
     public function __construct(string $templatePath)
     {
         $this->templatePath = $templatePath;
     }
 
-    public function render(string $template, $data = null): void
-    {
-        if ($data !== null) {
-            extract($data);
-        }
-        include $this->templatePath . $template . ".php";
-    }
+    abstract public function render(string $template, $data = null): void;
 }
