f74f43e9461317bb03a6767a8aef4c9c176ea2d6
DzianCPP <ocherkiprozaika@gmail.com>
2022-11-24 14:21:53 +0300

    Merged scripts from Task2.2

--- public/assets/scripts/users/edit.js
+++ public/assets/scripts/users/edit.js
@@ -1,20 +1,20 @@
-function sendPutRequest() {
-    const xhttp = new XMLHttpRequest();
+document.getElementById("submit-button").addEventListener("click", sendPutRequest);
 
-    let email = document.getElementById("email").value;
-    let fullName = document.getElementById("name").value;
-    let gender = document.getElementById("gender").value;
-    let status = document.getElementById("status").value;
-    let userID = document.getElementById("user-id").value;
+function sendPutRequest() {
+    let newEmail = document.getElementById("email").value;
+    let newFullName = document.getElementById("name").value;
+    let newGender = document.getElementById("gender").value;
+    let newStatus = document.getElementById("status").value;
+    let newUserID = document.getElementById("user-id").value;
 
     let url = "/user/update";
 
     let newUserInfo = {
-        newEmail: email,
-        newFullName: fullName,
-        newGender: gender,
-        newStatus: status,
-        newUserID: userID
+        email: newEmail,
+        fullName: newFullName,
+        gender: newGender,
+        status: newStatus,
+        userID: newUserID
     };
 
     let putRequest = {
