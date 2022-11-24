d13987339146ac046937debf3c5f6ac7524a5f67
DzianCPP <ocherkiprozaika@gmail.com>
2022-11-24 17:57:15 +0300

    Refactored a default  into a local variable

--- core/application/Database.php
+++ core/application/Database.php
@@ -6,7 +6,6 @@ use PDO;
 class Database
 {
     private PDO $connection;
-    private array $env = [];
     private static $instance = null;
 
     public static function getInstance(): Database
@@ -19,11 +18,11 @@ class Database
 
     private function __construct()
     {
-        $this->env = DotEnver::getDotEnv();
-        $dbHostName = $this->env['DB_HOST_NAME'];
-        $dbPassword = $this->env['DB_PASSWORD'];
-        $dbUserName = $this->env['DB_USER_NAME'];
-        $dbName = $this->env['DB_NAME'];
+        $env = DotEnver::getDotEnv();
+        $dbHostName = $env['DB_HOST_NAME'];
+        $dbPassword = $env['DB_PASSWORD'];
+        $dbUserName = $env['DB_USER_NAME'];
+        $dbName = $env['DB_NAME'];
         $dsn = "mysql:host=".$dbHostName.";dbname=".$dbName;
 
         $this->connection = new PDO($dsn, $dbUserName, $dbPassword);
