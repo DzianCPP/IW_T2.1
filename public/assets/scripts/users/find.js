7398c62a7d267d5a7c06c078faa82974e256f47a
DzianCPP <ocherkiprozaika@gmail.com>
2022-11-24 12:55:08 +0300

    Merged scripts from Task2.2

--- public/assets/scripts/users/find.js
+++ public/assets/scripts/users/find.js
@@ -1,3 +1,6 @@
+let findButton = document.getElementById("main-page-find-button");
+findButton.addEventListener("click", findUserById);
+
 function findUserById()
 {
     let input = document.getElementById("main-input-userID");
