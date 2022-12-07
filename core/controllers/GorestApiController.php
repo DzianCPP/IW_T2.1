ff5f6b178258cb39809a4166c329626ccb5d80a4
DzianCPP <ocherkiprozaika@gmail.com>
2022-12-07 15:38:29 +0300

    minor fix

--- core/controllers/GorestApiController.php
+++ core/controllers/GorestApiController.php
@@ -5,7 +5,6 @@ namespace core\controllers;
 abstract class GorestApiController
 {
     private const API_BASE_URI = "https://gorest.co.in";
-    private const API_AUTH_TOKEN = "'Authorization: Bearer ";
     private static $curlHandler;
 
     public static function getRecords(string $request_uri): array
@@ -29,6 +28,7 @@ abstract class GorestApiController
     private static function setGetCurl(string $request_uri): void
     {
         self::setCurlResource();
+        curl_setopt(self::$curlHandler, CURLOPT_FRESH_CONNECT, true);
         curl_setopt(self::$curlHandler, CURLOPT_RETURNTRANSFER, true);
         curl_setopt(self::$curlHandler, CURLOPT_URL, self::API_BASE_URI . $request_uri);
     }
