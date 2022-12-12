a3d206a1b8a8cdd1c155d739d645f9d60632e768
DzianCPP <ocherkiprozaika@gmail.com>
2022-12-12 11:45:53 +0300

    refactored userID into id in DB

---
+++ commands/migrateDB copy.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__ . '/../bootstrap/base-paths.php';
+require_once BASE_PATH . 'vendor/autoload.php';
+
+use system\Migrations;
+
+$migrations = new Migrations();
+$migrations->run();
