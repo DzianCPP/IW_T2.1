9a0ce49abbaf0bf6510c31a28cccb576da4eee80
DzianCPP <ocherkiprozaika@gmail.com>
2022-12-13 14:02:54 +0300

    swagger

--- core/models/UsersApiModel.php
+++ core/models/UsersApiModel.php
@@ -3,7 +3,13 @@
 namespace core\models;
 
 use core\GorestCurlBuilder;
+use OpenApi\Attributes as OA;
 
+ #[OA\Info(
+        title: "gorest API",
+        description: "Free REST API service",
+        version: "2.0")
+]
 class UsersApiModel implements ModelInterface
 {
     private GorestCurlBuilder $gorestCurlBuilder;
@@ -13,11 +19,60 @@ class UsersApiModel implements ModelInterface
         $this->gorestCurlBuilder = new GorestCurlBuilder();
     }
 
+    #[OA\Get(
+        path: "/public/v2/users",
+        summary: "get list of users",
+        responses: [
+            new OA\Response(response: 200, description: "OK"),
+            new OA\Response(response: 400, description: "Bad request"),
+            new OA\Response(response: 404, description: "Requested resource not found"),
+            new OA\Response(response: 405, description: "Method not allowed"),
+            new OA\Response(response: 429, description: "Too many requests"),
+            new OA\Response(response: 500, description: "Internal server error")
+        ],
+
+        security: [
+            new OA\SecurityScheme(
+                securityScheme: "bearerAuth",
+                type: "http",
+                scheme: "bearer"
+            )
+        ]
+    )]
+
     public function getUsers(array $columnValue = []): array
     {
         $result = $this->gorestCurlBuilder->executeCurl(method: "GET", id: $columnValue['value']);
         return json_decode($result, true);
     }
+
+    #[OA\Post(
+        path: "/public/v2/users",
+        summary: "create new user",
+        responses: [
+            new OA\Response(response: 200, description: "OK"),
+            new OA\Response(response: 201, description: "A resource was successfully created"),
+            new OA\Response(response: 400, description: "Bad request"),
+            new OA\Response(response: 401, description: "Authentication failed"),
+            new OA\Response(response: 404, description: "Requested resource not found"),
+            new OA\Response(response: 405, description: "Method not allowed"),
+            new OA\Response(response: 422, description: "Data validation failed"),
+            new OA\Response(response: 429, description: "Too many requests"),
+            new OA\Response(response: 500, description: "Internal server error")
+        ],
+
+        security: [
+            new OA\SecurityScheme(
+                securityScheme: "bearerAuth",
+                type: "http",
+                scheme: "bearer"
+            )
+        ],
+
+        parameters: [
+            
+        ]
+    )]
 
     public function create(): bool
     {
---
+++ openapi/openapi.php
@@ -0,0 +1,19 @@
+<?php
+
+use OpenApi\Processors\DocBlockDescriptions;
+use OpenApi\Processors\MergeIntoComponents;
+use OpenApi\Processors\MergeIntoOpenApi;
+
+require_once '../bootstrap/base-paths.php';
+require_once '../vendor/autoload.php';
+
+$validate = true;
+$logger = new \Psr\Log\NullLogger();
+$oa = (new \OpenApi\Generator($logger))
+    ->setAliases(['OA' => 'OpenApi\\Attributes'])
+    ->setNamespaces(['OpenApi\\Attributes'])
+    ->setAnalyser(new \OpenApi\Analysers\TokenAnalyser())
+    ->generate(
+        sources: ['../core/models/UsersApiModel.php'],
+        validate: $validate,
+        analysis: new \OpenApi\Analysis());
