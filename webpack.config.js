c08ea665d9cc6a26fd0ea238d0b8f6d46d5b94be
DzianCPP <ocherkiprozaika@gmail.com>
2022-12-15 13:35:37 +0300

    refactoring

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,17 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
-    entry: './src/assets/jquery/index.js',
+    entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, './public/assets/jquery'),
-        filename: 'jquery.js'
+        path: path.resolve(__dirname, 'public/assets/jquery'),
+        filename: 'main.js'
     },
 
     plugins: [
-        new HtmlWebpackPlugin(),
         new webpack.ProvidePlugin({
           $: 'jquery',
           jQuery: 'jquery'
         })
-    ],
-
-    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
-  }
+    ]
+}
